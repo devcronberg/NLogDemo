2c68991cf104d23905bdd24e5eaac2c3967a33af
Admin <Admin@AR-114-INS>
2021-10-14 14:54:49 +0200

    Add project files.

---
+++ NLogDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using NLog;
+using System;
+
+namespace LogDemo
+{
+    class Program
+    {
+        // https://nlog-project.org/
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+
+            logger.Trace("Trace...");
+            logger.Debug("Debug...");
+            logger.Info("Info...");
+            logger.Warn("Warn...");
+            logger.Error("Error...");
+
+            try
+            {
+                string a = null;
+                a.ToCharArray();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);                
+            }
+
+            logger.Fatal("Fatal...");
+
+            Metode1();
+
+        }
+
+        static void Metode1() {
+            logger.Debug("I metode1");
+            //
+            logger.Debug("Ud af metode1");
+        }
+        
+    }
+}
